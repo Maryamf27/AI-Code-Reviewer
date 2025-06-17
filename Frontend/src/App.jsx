e5d2e6c73a96263fd9131c617c14865b0d0d6998
Maryam Fatima <maryam27cs@gmail.com>
2025-06-17 11:28:33 +0500

    Add files via upload

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,55 @@
+import Prism from 'prismjs';
+import axios from 'axios';
+import Markdown from 'react-markdown'
+import 'prismjs/themes/prism-tomorrow.css';
+import "prismjs/components/prism-javascript";
+import Editor from 'react-simple-code-editor'
+import './App.css'
+import { useState, useEffect } from "react"
+
+
+
+function App() {
+  const [review, setReview] = useState(``)
+  const [code, setCode] = useState(` function sum() {
+                return 1+1;
+                }`)
+
+  useEffect(() => {
+    Prism.highlightAll();
+  });
+
+  async function codeReview() {
+    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+    setReview(response.data);
+  }
+  return (
+    <>
+      <main>
+        <div className="left">
+          <div className="code">
+            <Editor
+              value={code}
+              onValueChange={setCode}
+              highlight={code => Prism.highlight(code, Prism.languages.javascript, 'javascript')}
+              padding={10}
+              style={{
+                height: "100%",
+                width: "100%",
+                border: "1px solid #ddd",
+                borderRadius: "5px",
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 16,
+                backgroundColor: "#282c34"
+              }}
+            />
+          </div>
+          <div onClick={codeReview} className="review-btn">Review</div>
+        </div>
+        <div className="right"><Markdown>{review}</Markdown></div>
+      </main>
+    </>
+  )
+}
+
+export default App
